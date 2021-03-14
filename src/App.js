f5beebd9beee45ffc3bf928247c025da8f5f6365
Sergei Mamykin <mamykin.sergei@gmail.com>
2021-03-14 20:49:26 +0300

    setup firebase get/set user data. Up to lesson 102

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component
 {
@@ -20,9 +20,8 @@ class App extends React.Component
   unsubscribeFromAuth = () => {};
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user);
     });
   }
 
