3d5f55ebffe865987dc002e34eca216ca3460caf
Sergei Mamykin <mamykin.sergei@gmail.com>
2021-04-04 15:44:18 +0300

    firestore | signin & signup

--- src/App.js
+++ src/App.js
@@ -20,8 +20,23 @@ class App extends React.Component
   unsubscribeFromAuth = () => {};
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+
+        console.log({currentUser: userAuth});
+      } else {
+        this.setState({currentUser: userAuth});
+      }
     });
   }
 
